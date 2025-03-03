bf8af843a0624b1646f544c72623f463e2a4f568
anndrem <dealmeida.andre08@gmail.com>
2025-03-03 12:25:58 -0300

    organizando o menu responsivo

--- scr/menu-responsive.js
+++ scr/menu-responsive.js
@@ -4,23 +4,24 @@ const $ = (element) => {
 
 const menuPai = $('.menu-list');
 const ul = $('.list');
+const icon_btn = $('.open-list-responsive')
 const li = document.querySelectorAll('.item')
-const body = document.body.innerHTML
-var w_body = document.body.clientWidth
-
-if (menuPai.style.width == '5%') {
-    menuPai.addEventListener('click', () => {
-        menuPai.style.width = '18%';
-        li.forEach((element) => {
+var opacity_body = document.body.style.opacity
+icon_btn.addEventListener('click', () => {
+    if (menuPai.style.width <= '12%' && menuPai.style.height <= '5%') {
+        menuPai.style.width = '20%';
+        menuPai.style.height = '100%';
+        li.forEach(element => {
             element.style.display = 'block'
         })
-    })
-} else if (w_body <= '500px') {
-    menuPai.addEventListener('click', () => {
-        menuPai.style.width = '5%';
-        li.forEach((element) => {
+        opacity_body = '0.5'
+        console.log(Number(opacity_body))
+
+    } else {
+        menuPai.style.width = '12%';
+        menuPai.style.height = '5%';
+        li.forEach(element => {
             element.style.display = 'none'
         })
-    })
-}
-
+    }
+})
